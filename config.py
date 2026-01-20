bff2106c02cec35ed3e6b86563f1f67aca3c6a63
GojoSatoru4590 <ariyansatapathy@gmail.com>
2026-01-20 21:35:16 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,74 @@
+# +++ Made By Sanjiii [telegram username: @Urr_Sanjiii] +++
+
+import asyncio
+import os
+import logging
+from logging.handlers import RotatingFileHandler
+
+
+#Bot token @Botfather, --⚠️ REQUIRED--
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+#Your API ID from my.telegram.org --⚠️ REQUIRED--
+APP_ID = int(os.environ.get("APP_ID", "24371796"))
+
+#Your API Hash from my.telegram.org, --⚠️ REQUIRED--
+API_HASH = os.environ.get("API_HASH", "8121c78f4b8b31e88cc2623d1277338d")
+
+#Your db channel Id --⚠️ REQUIRED--
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1003511623672"))
+
+#OWNER ID --⚠️ REQUIRED--
+OWNER_ID = int(os.environ.get("OWNER_ID", "1683225887"))
+
+#SUPPORT_GROUP: This is used for normal users for getting help if they don't understand how to use the bot --⚠ OPTIONAL--
+SUPPORT_GROUP = os.environ.get("SUPPORT_GROUP", "-1002738802582")
+
+#Port
+PORT = os.environ.get("PORT", "8040")
+
+#Database --⚠️ REQUIRED--
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://sn117020:g3tULq1KLqxgzfgd@cluster0.ju3tzdx.mongodb.net/?retryWrites=true&w=majority&appName=DekuFuryBot")
+DB_NAME = os.environ.get("DATABASE_NAME", "Deku_Fury_Bot")
+
+VERIFY_DB = os.environ.get("VERIFY_DB", "mongodb+srv://adamopytbusiness1:uSswEjo4ZHMGDU8Z@cluster0.gqgmk.mongodb.net/?retryWrites=true&w=majority")
+DBV_NAME = os.environ.get("VERIFY_DBNAME", "Deku_FuryBot")
+
+
+TOKEN_PIC = os.environ.get("TOKEN_PIC", "https://graph.org/file/96f42ea1ef1da9b0aa8dc-d4c5a7493a8f465db5.jpg")
+
+
+#Tutorial video for the user of your shortner on how to download.
+TUT_VID = os.environ.get("TUT_VID","https://t.me/How_to_Download_7x/32")
+
+
+START_PIC = os.environ.get("START_PIC", "https://graph.org/file/f47115fca468d2729ec46-82018df15c60913e58.jpg")
+FORCE_PIC = os.environ.get("FORCE_PIC", "https://graph.org/file/5af4fa39d2d3d6c98666a-1598c902617abd16ea.jpg")
+
+                        
+TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
+
+#Collection of pics for Bot // #Optional but atleast one pic link should be replaced if you don't want predefined links
+PICS = (os.environ.get("PICS", "https://graph.org/file/248175684faf82eff03c6-2300075c6cd4b1d5bf.jpg")).split() #Required
+#set your Custom Caption here, Keep None for Disable Custom Caption
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
+
+LOG_FILE_NAME = "filesharingbot.txt"
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s - %(levelname)s] - %(name)s - %(message)s",
+    datefmt='%d-%b-%y %H:%M:%S',
+    handlers=[
+        RotatingFileHandler(
+            LOG_FILE_NAME,
+            maxBytes=50000000,
+            backupCount=10
+        ),
+        logging.StreamHandler()
+    ]
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+def LOGGER(name: str) -> logging.Logger:
+    return logging.getLogger(name)
